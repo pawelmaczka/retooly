1c00427e41ff9b313dfb4a6a2dcd54a8bd0a8730
pawelmaczka <pawel@maczka.it>
2021-10-20 18:27:52 +0200

    update Component generator

--- src/plopfile.js
+++ src/plopfile.js
@@ -1,16 +1,21 @@
 module.exports = function plopMain(plop) {
   const currentPath = process.env.INIT_CWD;
 
-  plop.setGenerator('component', {
-    description: 'application component logic',
+  plop.setGenerator('Component', {
+    description: 'Create component in current directory',
     prompts: [
       {
         type: 'input',
         name: 'name',
-        message: 'component name please',
+        message: 'Provide component name',
       },
     ],
     actions: [
+      {
+        type: 'add',
+        path: `${currentPath}/index.js`,
+        templateFile: 'templates/index.hbs',
+      },
       {
         type: 'add',
         path: `${currentPath}/{{name}}.js`,
